cfa99204492a8d8e2b8bb426de9291e329d9e178
KgatlisoMkhwanazi <KgatlisoMkhwanazi@MON-PW0B0ZJQ>
2024-06-27 09:22:28 +0200

    Add project files.

---
+++ dbo/Tables/Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]        INT           NOT NULL,
+    [firstname] NVARCHAR (50) NULL,
+    [lastname]  NVARCHAR (50) NULL,
+    [age]       INT           NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
